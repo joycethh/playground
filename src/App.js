5f742dc69b8168b64c8b50dacb8611c2bf37f1b6
joycethh <joycethh@outlook.com>
2023-01-25 14:45:52 -0600

    reduce import path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import Header from "./components/header/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Header, Footer } from "./components/index";
+import { Home, Contact, Admin, Cart } from "./pages/index";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/Cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./home/Home";
+export { default as Contact } from "./contact/Contact";
+export { default as Admin } from "./admin/Admin";
+export { default as Cart } from "./cart/Cart";

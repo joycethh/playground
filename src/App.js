b5402b291b9128b75a991fb96aa8cb7cc8ff4429
joycethh <joycethh@outlook.com>
2023-02-01 12:38:44 -0600

    add theme context to the app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import { Navbar, Footer } from "./components/index";
 import { Home, Contact, Admin, Cart } from "./pages/index";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+  const { isDarkMode } = useContext(DarkModeContext);
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/Cart" element={<Cart />} />
-        </Routes>
-        <Footer />
+        <div className={`theme-${isDarkMode ? "dark" : "light"}`}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/Cart" element={<Cart />} />
+          </Routes>
+          <Footer />
+        </div>
       </BrowserRouter>
     </>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { BiUser } from "react-icons/bi";
 import { CiDark, CiCircleList } from "react-icons/ci";
 import { CgShoppingBag } from "react-icons/cg";
-import { HiOutlineSearch, HiOutlineLightBulb } from "react-icons/hi";
+import {
+  HiOutlineSearch,
+  HiOutlineLightBulb,
+  HiMenuAlt1,
+} from "react-icons/hi";
 import { DarkModeContext } from "../../context/darkModeContext";
-import "./style.scss";
+import "./navbar.scss";
 
 const Navbar = () => {
   const { toggle, isDarkMode } = useContext(DarkModeContext);
-  // const [currentUser, setCurrentUser] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = () => {
+    setOpenModal(!openModal);
+  };
+
+  const hideModal = () => {
+    setOpenModal(false);
+  };
   return (
     <header>
       <div className="navbar">
@@ -41,6 +53,35 @@ const Navbar = () => {
           <span onClick={toggle}>
             {isDarkMode ? <HiOutlineLightBulb /> : <CiDark />}
           </span>
+
+          <span onClick={toggleModal} className="menu-icon">
+            <HiMenuAlt1 />
+          </span>
+        </div>
+
+        <div
+          className={openModal ? "modal display-block" : "modal display-none"}
+        >
+          <button type="button" onClick={toggleModal}>
+            Close Menu
+          </button>
+          <div className="modal">
+            <ul>
+              <li>New Arrivals</li>
+              <li>Best Sellers</li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>
+                <BiUser />
+              </li>
+              <li>
+                <span onClick={toggle}>
+                  {isDarkMode ? <HiOutlineLightBulb /> : <CiDark />}
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>

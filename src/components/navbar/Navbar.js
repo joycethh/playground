2be7a8d1a48935ebd0eeb8d71ae32202b436b9bf
joycethh <joycethh@outlook.com>
2023-02-01 16:51:00 -0600

    complete mobile nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -56,18 +56,16 @@ const Navbar = () => {
             <BiMenuAltLeft />
           </span>
         </div>
+      </div>
 
-        <div
-          className={openModal ? "modal display-block" : "modal display-none"}
-        >
-          <div className="modal-nav">
-            <div className="modal-nav-wrapper">
-              <Link to="/new-arrivals">New Arrivals</Link>
-              <Link to="/best-sellers">Best Sellers</Link>
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact</Link>
-            </div>
+      <div className={openModal ? "modal display-block" : "modal display-none"}>
+        <div className="modal-nav">
+          <div className="modal-nav-wrapper" onClick={toggleModal}>
+            <Link to="/new-arrivals">New Arrivals</Link>
+            <Link to="/best-sellers">Best Sellers</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
           </div>
         </div>
       </div>

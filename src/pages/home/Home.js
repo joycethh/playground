ca2d52f36012fa18bdce4ac5a904f0ab95018aa4
joycethh <joycethh@outlook.com>
2023-02-07 17:28:43 -0600

    add grid layout to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,27 @@
 import React from "react";
-import "./style.scss";
+import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section>
+        <div className="flex-grid">
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+          <div className="col">one</div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;

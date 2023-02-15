44b69e5d6d1cf50de432b047b1f3bd73ed909552
joycethh <joycethh@outlook.com>
2023-02-14 22:12:51 -0600

    create loader component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./home.scss";
+import Loader from "../../components/loader/Loader";
 
 const Home = () => {
   return (
     <main>
       <section>
+        <div className="loader">
+          <Loader />
+        </div>
         <div className="flex-grid">
           <div className="col">one</div>
           <div className="col">one</div>

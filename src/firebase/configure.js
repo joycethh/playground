48c14322539b6dd7e4ddee96ac32cd78c7b83d4d
joycethh <joycethh@outlook.com>
2023-02-14 21:39:50 -0600

    set up with firebase

--- src/firebase/configure.js
+++ src/firebase/configure.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDuRfKM7orwtRL8OOeTwkU4GAwxAk70YMY",
   authDomain: "seashell-53beb.firebaseapp.com",
   projectId: "seashell-53beb",
@@ -16,6 +13,9 @@ const firebaseConfig = {
   measurementId: "G-6REF42JG6K",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;

ecdfc3109b859824be9fdbbcc47193ba58e7f08f
joycethh <joycethh@outlook.com>
2023-02-14 21:36:25 -0600

    install firebase

---
+++ src/firebase/configure.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuRfKM7orwtRL8OOeTwkU4GAwxAk70YMY",
+  authDomain: "seashell-53beb.firebaseapp.com",
+  projectId: "seashell-53beb",
+  storageBucket: "seashell-53beb.appspot.com",
+  messagingSenderId: "758671265377",
+  appId: "1:758671265377:web:b47e3a1fb8db6069126420",
+  measurementId: "G-6REF42JG6K",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);

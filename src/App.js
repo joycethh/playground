24c53ebb0645f04ec62178a73f7db095d70c37dd
joycethh <joycethh@outlook.com>
2023-02-21 11:38:58 -0600

    create router and protected route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import { useContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Navbar, Footer } from "./components/index";
-import {
-  Home,
-  Contact,
-  Admin,
-  Cart,
-  NewArrivals,
-  BestSellers,
-  Auth,
-  Reset,
-} from "./pages/index";
+import Routers from "./routers/Routers";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { DarkModeContext } from "./context/darkModeContext";
@@ -23,16 +15,7 @@ function App() {
         <div className={`theme-${isDarkMode ? "dark" : "light"}`}>
           <ToastContainer />
           <Navbar />
-          <Routes>
-            <Route path="/new-arrivals" element={<NewArrivals />} />
-            <Route path="/best-sellers" element={<BestSellers />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/contact-us" element={<Contact />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/reset-password" element={<Reset />} />
-          </Routes>
+          <Routers />
           <Footer />
         </div>
       </BrowserRouter>

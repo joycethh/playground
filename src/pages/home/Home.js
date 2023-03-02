2aaa42bf8c549d315a60e7ecee42d8f4a8df56e5
joycethh <joycethh@outlook.com>
2023-03-02 15:41:48 -0600

    add shop link to the button

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./home.scss";
 // import { Loader } from "../../components/index";
 import Badge from "../../components/badge/Badge";
@@ -14,6 +15,20 @@ const Home = () => {
           <section className="hero-section">
             <div className="banner-wrapper">
               <img src={banner} alt="" />
+              <div className="description">
+                <span>Aqua treasures are waiting for you</span>
+                <h2>Adorn yourself with the natural beauty of pearls</h2>
+                <p>
+                  These treasures of the water and ocean exude elegance and
+                  sophistication, lending a touch of glamour to any outfit.
+                </p>
+
+                <div className="shop-now">
+                  <button>
+                    <Link to="/shop">Shop Now</Link>
+                  </button>
+                </div>
+              </div>
             </div>
           </section>
           <section className="products-section">

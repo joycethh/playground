1d744eb3dec9cfc3037fe019c5fd1b2f18a3fc4c
joycethh <joycethh@outlook.com>
2023-03-22 15:59:14 -0500

    create category section on home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 import "./home.scss";
 // import { Loader } from "../../components/index";
 import Badge from "../../components/badge/Badge";
-
 import ProductList from "../../components/ui/ProductList";
 import useProductData from "../../customHooks/useProductData";
+import Categories from "./Categories";
+import { LinkButton } from "../../components/button/Button";
 
 const Home = () => {
   const { apiData } = useProductData();
@@ -22,15 +23,19 @@ const Home = () => {
                   <span>Aqua treasures are waiting for you</span>
                   <h2>Adorn yourself with the natural beauty of pearls</h2>
                   <div className="shop-now">
-                    <button>
+                    {/* <button>
                       <Link to="/shop">Shop Now</Link>
-                    </button>
+                    </button> */}
+                    <LinkButton name="Shop Now" path="/shop" />
                   </div>
                 </div>
               </div>
               <div></div>
             </div>
           </div>
+        </section>
+        <section className="categories-section">
+          <Categories />
         </section>
         {/* <section className="products-section">
           <ProductList data={apiData} />

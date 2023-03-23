06e512004ef74d20ee0c36ce99db0c4b079b07e3
joycethh <joycethh@outlook.com>
2023-03-22 22:34:11 -0500

    completed featured products pr

--- src/pages/home/FeaturedProduct.js
+++ src/pages/home/FeaturedProduct.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import useProductData from "../../customHooks/useProductData";
-
+import "./featuredProduct.scss";
 import ProductList from "../../components/ui/ProductList";
 
 const FeaturedProducts = () => {
@@ -16,9 +16,7 @@ const FeaturedProducts = () => {
   return (
     <div className="pt-3">
       <div className="page-width">
-        <h3 className="mt-3 mb-3 text-center uppercase h5 tracking-widest">
-          Featured collection
-        </h3>
+        <h3 className="mt-3 mb-3">Featured collection</h3>
         <ProductList data={products} />
       </div>
     </div>

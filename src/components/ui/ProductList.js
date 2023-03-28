784043b950cf06159c968ecf7d9897e218a75c95
joycethh <joycethh@outlook.com>
2023-03-28 17:12:55 -0500

    update product data

--- src/components/ui/ProductList.js
+++ src/components/ui/ProductList.js
@@ -1,19 +1,3 @@
-// import React from "react";
-// import ProductCard from "./ProductCard";
-// import "./product.scss";
-
-// const ProductList = ({ data }) => {
-//   return (
-//     <>
-//       <div className="flex-grid">
-//         {data && data.map((item, key) => <ProductCard item={item} key={key} />)}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ProductList;
-
 import React from "react";
 import ProductCard from "./ProductCard";
 import "./product.scss";

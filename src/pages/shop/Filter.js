10e5289926a213880aa2c5efb7ef299374b7c161
joycethh <joycethh@outlook.com>
2023-03-28 11:11:16 -0500

    implete the filter by category component

--- src/pages/shop/Filter.js
+++ src/pages/shop/Filter.js
@@ -1,78 +1,39 @@
-import React, { useState } from "react";
-import { RiSearch2Line } from "react-icons/ri";
-import useProductData from "../../customHooks/useProductData";
-import ProductList from "../../components/ui/ProductList";
+import React from "react";
+// import { RiSearch2Line } from "react-icons/ri";
+// import Input from "../../components/input/Input";
 import "./filter.scss";
-import Input from "../../components/input/Input";
 
-const Filter = () => {
-  const { apiData } = useProductData();
-  const [filterData, setFilterData] = useState(apiData);
+const Filter = ({ filterValue, handleSelect }) => {
+  const category = [
+    { id: 1, name: "jewelry" },
+    { id: 2, name: "electronics" },
+    { id: 3, name: `women's clothing` },
+    { id: 4, name: `men's clothing` },
+  ];
 
-  const handleFilter = (e) => {
-    const filterVal = e.target.value;
-    if (filterVal === "nan") setFilterData(apiData);
-    if (filterVal === "jewelery") {
-      const filltered =
-        apiData && apiData.filter((item) => item.category === "jewelery");
-
-      setFilterData(filltered);
-    }
-
-    if (filterVal === "electronics") {
-      const filltered =
-        apiData && apiData.filter((item) => item.category === "electronics");
-
-      setFilterData(filltered);
-    }
-
-    if (filterVal === "women's clothing") {
-      const filltered =
-        apiData &&
-        apiData.filter((item) => item.category === `women's clothing`);
-
-      setFilterData(filltered);
-    }
-
-    if (filterVal === "men's clothing") {
-      const filltered =
-        apiData && apiData.filter((item) => item.category === `men's clothing`);
-
-      setFilterData(filltered);
-    }
-  };
-
-  const handleSearch = (e) => {
-    const searchVal = e.target.value;
-
-    const searchedItem = apiData.filter((item) =>
-      item.title.toLowerCase().includes(searchVal.toLowerCase())
-    );
-
-    setFilterData(searchedItem);
-  };
   return (
     <>
       <section className="filter-section">
         <div className="filterSection-container">
           <div className="filter-left">
             <div className="select-wrapper">
-              <select onChange={handleFilter}>
-                <option onChange={handleFilter}>Product Type</option>
-                <option value="jewelery">jewelery</option>
-                <option value="electronics">electronics</option>
-                <option value="women's clothing">women's clothing</option>
-                <option value="men's clothing">men's clothing</option>
+              <label htmlFor="category-select">Filter by Category:</label>
+              <select value={filterValue} onChange={handleSelect}>
+                {category.map((category) => (
+                  <option key={category.id} value={category.name}>
+                    {category.name}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="select-wrapper">
-              <select onChange={handleFilter}>
+              <select onChange={() => {}}>
                 <option value="ascending">ascending</option>
                 <option value="decending">decending</option>
               </select>
             </div>
           </div>
-          <div className="search-right">
+          {/* <div className="search-right">
             <Input
               name="search"
               onChange={handleSearch}
@@ -81,17 +42,8 @@ const Filter = () => {
               placeholder="Search..."
               icon={<RiSearch2Line />}
             />
-          </div>
+          </div> */}
         </div>
-      </section>
-      <section>
-        {filterData?.length ? (
-          <ProductList data={filterData} />
-        ) : (
-          <div className="info-wrapper">
-            <h2>Sorry, we do not have such product at the moment!</h2>
-          </div>
-        )}
       </section>
     </>
   );
--- src/pages/shop/Jewelry.js
+++ src/pages/shop/Jewelry.js
@@ -1,107 +1,37 @@
 import React, { useState } from "react";
 import CommonBanner from "../../components/ui/CommonBanner";
-import Badge from "../../components/badge/Badge";
-import { RiSearch2Line } from "react-icons/ri";
-import "./shop.scss";
 import useProductData from "../../customHooks/useProductData";
-import jewelryCommon from "../../assets/jewelry-white-common.jpg";
+import Badge from "../../components/badge/Badge";
+import Filter from "./Filter";
 import ProductList from "../../components/ui/ProductList";
+import jewelryCommon from "../../assets/jewelry-white-common.jpg";
 
 const Jewelry = () => {
   const { apiData } = useProductData();
+  const [category, setCategory] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(apiData);
 
-  const [filterData, setFilterData] = useState(apiData);
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    console.log("e.target.value", e.target.value);
+    console.log("category", category);
 
-  const handleFilter = (e) => {
-    const filterVal = e.target.value;
-    if (filterVal === "nan") setFilterData(apiData);
-    if (filterVal === "jewelery") {
-      const filltered =
-        apiData && apiData.filter((item) => item.category === "jewelery");
-
-      setFilterData(filltered);
+    if (category === "jewelry") {
+      setFilteredProducts(
+        apiData?.filter((product) => product.category === category)
+      );
     }
-
-    if (filterVal === "electronics") {
-      const filltered =
-        apiData && apiData.filter((item) => item.category === "electronics");
-
-      setFilterData(filltered);
-    }
-
-    if (filterVal === "women's clothing") {
-      const filltered =
-        apiData &&
-        apiData.filter((item) => item.category === `women's clothing`);
-
-      setFilterData(filltered);
-    }
-
-    if (filterVal === "men's clothing") {
-      const filltered =
-        apiData && apiData.filter((item) => item.category === `men's clothing`);
-
-      setFilterData(filltered);
-    }
+    setFilteredProducts(apiData);
   };
 
-  const handleSearch = (e) => {
-    const searchVal = e.target.value;
-
-    const searchedItem = apiData.filter((item) =>
-      item.title.toLowerCase().includes(searchVal.toLowerCase())
-    );
-
-    setFilterData(searchedItem);
-  };
+  console.log("filteredProducts", filteredProducts);
 
   return (
     <Badge title="shop">
       <CommonBanner imageUrl={jewelryCommon} title="jewelry" />
-      {/* <section className="filter-section">
-        <div className="filterSection-container">
-          <div className="filterBy-wrapper">Filter By:</div>
-          <div className="filterOption-wrapper">
-            <div className="select-wrapper">
-              <select onChange={handleFilter}>
-                <option onChange={handleFilter}>Product Type</option>
-                <option value="jewelery">jewelery</option>
-                <option value="electronics">electronics</option>
-                <option value="women's clothing">women's clothing</option>
-                <option value="men's clothing">men's clothing</option>
-              </select>
-            </div>
-
-            <div className="sort-wrapper">
-              <select onChange={handleFilter}>
-                <option value="ascending">ascending</option>
-                <option value="decending">decending</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="search-wrapper">
-            <input
-              name="search"
-              onChange={handleSearch}
-              type="text"
-              className="searchInput"
-              placeholder="Search..."
-            />
-            <span>
-              <RiSearch2Line />
-            </span>
-          </div>
-        </div>
-      </section> */}
+      <Filter filterValue="jewelry" handleSelect={handleCategoryChange} />
       <section>
-        {filterData?.length ? (
-          <ProductList data={filterData} />
-        ) : (
-          <div className="info-wrapper">
-            <h2>Sorry, we do not have such product at the moment!</h2>
-          </div>
-        )}
+        <ProductList data={filteredProducts} />
       </section>
     </Badge>
   );

97c80fb28a0a57900d5b0988b1871878db2d5c20
joycethh <joycethh@outlook.com>
2023-03-31 10:54:49 -0500

    add margin bottom to productLists

--- src/pages/home/FeaturedProduct.js
+++ src/pages/home/FeaturedProduct.js
@@ -8,7 +8,7 @@ const FeaturedProducts = () => {
 
   return (
     <div className="pt-3">
-      <div className="page-width">
+      <div className="page-width mb-3">
         <h3 className="mt-3 mb-3">Featured collection</h3>
         {apiData && <ProductList products={apiData} />}
       </div>
--- src/pages/shop/Jewelry.js
+++ src/pages/shop/Jewelry.js
@@ -28,20 +28,26 @@ const Jewelry = () => {
   return (
     <Badge title="Jewelry">
       <CommonBanner imageUrl={banner} title="Jewelry" />
-      <Filter
-        categories={categories}
-        selectedCategory={selectedCategory}
-        searchQuery={searchQuery}
-        onSelectCategory={handleSelectCategory}
-        onSearchQuery={handleSearchTitle}
-      />
-      <section>
-        <ProductList
-          products={apiData}
+      <div className="mt-15">
+        <Filter
+          categories={categories}
           selectedCategory={selectedCategory}
           searchQuery={searchQuery}
+          onSelectCategory={handleSelectCategory}
+          onSearchQuery={handleSearchTitle}
         />
-      </section>
+        <section>
+          <div className="pt-3">
+            <div className="page-width mb-3">
+              <ProductList
+                products={apiData}
+                selectedCategory={selectedCategory}
+                searchQuery={searchQuery}
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </Badge>
   );
 };

81102a1f61ee6df7aed0b96c0d57a8a82b2148c1
joycethh <joycethh@outlook.com>
2023-04-05 21:58:03 -0500

    use customform

--- src/components/footer/email/Email.js
+++ src/components/footer/email/Email.js
@@ -1,31 +1,53 @@
 import React, { useState } from "react";
 import { HiOutlineArrowRight } from "react-icons/hi";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 import "./email.scss";
 
 const Email = () => {
   const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("");
 
-  const handleChange = (e) => {
-    setEmail(e.target.value);
-  };
+  const url = "";
+
   // console.log("email in footer", email);
   return (
-    <div className="subscription">
-      <h3>Subscribe for new arrivals:</h3>
-      <div className="subscriptInputWrapper">
-        <input
-          name="email"
-          type="text"
-          value={email}
-          autoFocus
-          placeholder="Enter email to subscribe"
-          onChange={handleChange}
-        />
-        <span className="icon" onClick={() => {}}>
-          <HiOutlineArrowRight />
-        </span>
-      </div>
-    </div>
+    <MailchimpSubscribe
+      url={url}
+      render={({ subscribe, status, message }) => (
+        <div className="subscription">
+          <h3>Subscribe for new arrivals:</h3>
+          <form onSubmit={(email) => subscribe(email)}>
+            <div className="subscriptInputWrapper">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={email}
+                required
+                placeholder="Enter email to subscribe"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="icon" onClick={(email) => subscribe(email)}>
+                <HiOutlineArrowRight />
+              </span>
+            </div>
+          </form>
+
+          {status === "sending" && (
+            <div style={{ color: "blue" }}>sending...</div>
+          )}
+          {status === "error" && (
+            <div
+              style={{ color: "red" }}
+              dangerouslySetInnerHTML={{ __html: message }}
+            />
+          )}
+          {status === "success" && (
+            <div style={{ color: "green" }}>Subscribed !</div>
+          )}
+        </div>
+      )}
+    />
   );
 };
 
